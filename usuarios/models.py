2bf23cb12723ab1db48851e55517ff0b1a5af3ee
joaocastro0429 <joaocastro0429@gmail.com>
2025-07-26 21:39:14 -0300

    atualizando o projeto incluindo o admin do painel admin

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length = 64)
+    
+    def __str__(self) -> str:
+      return self.nome
--- usuarios/views.py
+++ usuarios/views.py
@@ -4,7 +4,7 @@ from django.http import  HttpResponse
 # Create your views here.
 
 def cadastro(request):
-    return HttpResponse('Cadastro')
+    return render(request, 'cadastro.html')
 
 def login (request):
-    return HttpResponse('Login')
+    return render(request,'login.html')
