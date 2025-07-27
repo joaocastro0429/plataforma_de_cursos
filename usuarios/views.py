0d24b57075dcfa3dbd94db4186e02257979c2749
joaocastro0429 <joaocastro0429@gmail.com>
2025-07-26 21:50:13 -0300

    personalizando o admin

--- usuarios/views.py
+++ usuarios/views.py
@@ -8,3 +8,14 @@ def cadastro(request):
 
 def login (request):
     return render(request,'login.html')
+
+def valida_cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        
+        # Aqui você pode adicionar lógica para salvar os dados do usuário no banco de dados
+        
+        return HttpResponse(f'Cadastro realizado com sucesso! Nome: {nome}, Email: {email}')
+   
